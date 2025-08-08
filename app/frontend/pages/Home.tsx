1c738ef49f64f3a235b72574054b2d773753ec19
toothtaste <197402001+toothtaste@users.noreply.github.com>
2025-08-09 00:45:21 +0700

    feat: placeholder

--- app/frontend/pages/Home.tsx
+++ app/frontend/pages/Home.tsx
@@ -1,7 +1,10 @@
 import { store } from "@/lib/store"
+import sdk from "@farcaster/miniapp-sdk"
 import { useQuery } from "@tanstack/react-query"
 import axios from "axios"
 import clsx from "clsx"
+
+// make txt ascii generation
 
 export default function Home() {
   const { user } = store()
@@ -13,18 +16,42 @@ export default function Home() {
     enabled: !!pfpUrl,
   })
 
+  const placeholder =
+    Array.from({ length: 4 })
+      .map(_ => "⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\n")
+      .join("") +
+    Array.from({ length: Math.floor((33 - user?.username?.length!) / 2) + 1 })
+      .map(_ => "⣿")
+      .join("") +
+    " " +
+    user?.username +
+    " " +
+    Array.from({ length: Math.floor((33 - user?.username?.length!) / 2) + 1 })
+      .map(_ => "⣿")
+      .join("") +
+    "\n" +
+    Array.from({ length: 4 })
+      .map(_ => "⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\n")
+      .join("") +
+    "\n"
+
   return (
     <main className={clsx("fixed top-5 inset-x-0 bottom-0", "overflow-hidden")}>
       <div className={clsx("flex justify-center")}>
         <div className={clsx("whitespace-pre font-mono leading-none")}>{isLoading ? "isLoading..." : data}</div>
       </div>
 
-      {/* <button
-        onClick={() => sdk.actions.composeCast({ text: data, embeds: [`https://${process.env.NEXT_PUBLIC_HOST}`] })}
+      <button
+        onClick={() =>
+          sdk.actions.composeCast({
+            text: placeholder + data,
+            embeds: [`https://${process.env.NEXT_PUBLIC_HOST}`],
+          })
+        }
         className={clsx("absolute bottom-15 left-1/2 -translate-x-1/2", "text-lg lowercase")}
       >
         Share
-      </button> */}
+      </button>
     </main>
   )
 }
